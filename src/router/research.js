b9dcbf6235b568330f4ec5417e52a4dec7c46182
陈舟 <chenzhou@xuetangx.com>
2018-11-25 11:09:36 +0800

    项目详情页开发

--- src/router/research.js
+++ src/router/research.js
@@ -7,7 +7,8 @@ Vue.use(Router)
 export default new Router({
   mode: 'history',
   // mode: 'hash',
-  base: process.env.BASE_URL,
+  // base: process.env.BASE_URL,
+  base: '/',
   routes: [
     {
       path: '/',
